b76a5edc53ed745d6919d9e186a26c1ae7088f8b
Betty Jang <bettyjang1005@gmail.com>
2017-10-31 12:06:58 +0800

    1031 BMI message

--- app/src/main/java/com/bettyjang1005/bmi/MainActivity.java
+++ app/src/main/java/com/bettyjang1005/bmi/MainActivity.java
@@ -1,10 +1,12 @@
 package com.bettyjang1005.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +20,19 @@ public class MainActivity extends AppCompatActivity {
         //Log.d("MainActivity","testing bmi method");
         //Log.e("MainActivity","testing2 bmi method");
         //Log.i("MainActivity","testing3 bmi method");
+       // EditText edWeight = (EditText) findViewById(R.id.ed_weight);
         EditText edWeight = (EditText) findViewById(R.id.ed_weight);
         EditText edHeight = (EditText) findViewById(R.id.ed_height);
         float edweight = Float.parseFloat(edWeight.getText().toString());
         float edheight = Float.parseFloat(edHeight.getText().toString());
         float bmi = edweight/(edheight*edheight);
-        Log.d("MainActivity","Your BMI is : " + bmi);
-
-
-
+        new AlertDialog.Builder(this).setMessage("Your BMI is " + bmi).setTitle("BMI")
+                .setPositiveButton("OK",null).setNegativeButton("NO",null)
+                .setNeutralButton("Cancel",null)
+                .show();
+       /* Log.d("MainActivity","Your BMI is : " + bmi);
+        Toast.makeText(this , "Your BMI  is " + bmi ,Toast.LENGTH_SHORT).show();*/
 
     }
+
 }
