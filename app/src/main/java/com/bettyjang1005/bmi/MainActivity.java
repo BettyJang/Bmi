0a74814c8a604c155a701ebcdc182c02da458c71
Betty Jang <bettyjang1005@gmail.com>
2017-11-07 10:57:09 +0800

    OnClick Bmi

--- app/src/main/java/com/bettyjang1005/bmi/MainActivity.java
+++ app/src/main/java/com/bettyjang1005/bmi/MainActivity.java
@@ -5,15 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String s = new String("abc");
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getResources().getString(R.string.app_name);
+        Button help = (Button) findViewById(R.id.b_help);
+        help.setOnClickListener(listener);
+
     }
     public void bmi(View view) {
         //System.out.println("hi hi hi");
@@ -26,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         float edweight = Float.parseFloat(edWeight.getText().toString());
         float edheight = Float.parseFloat(edHeight.getText().toString());
         float bmi = edweight/(edheight*edheight);
-        new AlertDialog.Builder(this).setMessage("Your BMI is " + bmi).setTitle("BMI")
-                .setPositiveButton("OK",null).setNegativeButton("NO",null)
-                .setNeutralButton("Cancel",null)
+        new AlertDialog.Builder(this).setMessage(getString(R.string.Your_BMI_is_) + bmi).setTitle(R.string.BMI_title)
+                .setPositiveButton(R.string.ok,null).setNegativeButton(R.string.no,null)
+                .setNeutralButton(R.string.cancel,null)
                 .show();
        /* Log.d("MainActivity","Your BMI is : " + bmi);
         Toast.makeText(this , "Your BMI  is " + bmi ,Toast.LENGTH_SHORT).show();*/
